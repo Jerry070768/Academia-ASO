c7042ad680cb61a68a2b37a4c2bd6a0df175fa3f
Jerry070768 <159818889+Jerry070768@users.noreply.github.com>
2024-02-13 16:40:00 -0600

    Ejercicio 1 Interface funcional Biconsumer

--- Biconsumer.java
+++ Biconsumer.java
@@ -3,7 +3,10 @@ import java.util.function.BiConsumer;
 
 public class Main {
     public static void main(String[] args) {
-        BiConsumer<String, String> biConsumer = (s1, s2) -> System.out.println(s1 + ", " + s2);
+        String cadena1 = "Ejercicio 1 : Interface funcional Consumer ";
+        String cadena2 = "La longitud del texto es de: ";
+        String cadena3 = cadena2+cadena1.length();
+        BiConsumer<String, String> biConsumer = (s1, s2) -> System.out.println(cadena1 + ", " + cadena3);
         biConsumer.accept("Ejemplo de Interface Funcional", "Biconsumer");
         biConsumer.toString();
     }
