85eaa9962857005dd00bc8963eee35ada540185c
Jerry070768 <159818889+Jerry070768@users.noreply.github.com>
2024-02-13 16:18:22 -0600

    Ejercicio 1 Interface funcional consumer

--- Consumer.java
+++ Consumer.java
@@ -1,9 +1,11 @@
+import java.io.RandomAccessFile;
 import java.util.function.Consumer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String cadena1 = "Ejercicio 1 : Interface funcional Consumer ";
+        String cadena2 = "La longitud del texto es de: ";
         Consumer<String> consumer = (s) -> System.out.println(s);
-        consumer.accept("Ejercicio 1 : Interface funcional Consumer");
-        consumer.accept("Ejercicio 2 : Gerardo Rodríguez Trejo");
+        consumer.accept(cadena1+cadena2+cadena1.length());
     }
 }
